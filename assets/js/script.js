acdf0025533960f3d55253a66e0372134904d110
vini1cas <>
2024-01-24 21:05:54 +0000

    create the logic for the calcAnswer and checkAnswer functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     for (let button of buttons) {
         button.addEventListener("click", function () {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                calcCorrectAnswer();
             }
             else {
                 let gameType = this.getAttribute("data-type");
@@ -30,11 +30,30 @@ function runGame(gameType) {
 }
 
 function checkAnswer() {
+    let userAnswer = parseInt.document.getElementById('answer').value;
+    let calcAnswer = calcCorrectAnswer();
 
+    if (userAnswer === calcAnswer[0]) {
+        alert("Congrats! You got the right answer!");
+    } else {
+        alert(`Awwwww, shoot... You guessed ${userAnswer}. The correct answer was ${calcAnswer}.`);
+    }
+
+    runGame (calcAnswer[1]);
 }
 
-function calculateCorrectAnswer() {
+function calcCorrectAnswer() {
 
+    let operand1 = parseInt.document.getElementById('operand1').innerText;
+    let operand2 = parseInt.document.getElementById('operand2').innerText;
+    let operator = document.getElementById('operator').innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, 'addition']
+    } else {
+        alert(`Unimplemented operator: ${operator}`);
+        throw `Unimplemented operator: ${operator}`;
+    }
 }
 
 function incrScore() {
